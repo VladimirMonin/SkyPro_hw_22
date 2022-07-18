918e7713bf781bc4d545ac6f9804c10fc6ebad82
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-07-18 16:15:53 +0600

    Первое задание

--- part1/practice/abstract_classes/main.py
+++ part1/practice/abstract_classes/main.py
@@ -1,14 +1,14 @@
-# Создайте абстрактный класс автомобиля Transport c абстрактными методами
+# TODO Создайте абстрактный класс автомобиля Transport c абстрактными методами
 # - start_engine
 # - stop_engine
 # - move
 # - stop
 
-# Унаследуйте от него три класса Boat, Car, Electroscooter
+# TODO Унаследуйте от него три класса Boat, Car, Electroscooter
 # для каждого из требуемых методов через print укажите какое-либо действие.
 # К примеру start_engine -> print('Двигатель катера запущен')
 
-# Создайте класс Person у которого будет один единственный метод use_transport.
+# TODO Создайте класс Person у которого будет один единственный метод use_transport.
 # В данный метод в качестве параметра должен передаваться объект реализующий интерфейс Transport
 # Метод для этого объекта должен запускать двигатель, двигаться куда-то, тормозить и глушить двигатель.
 # Для текстовой анимации Вы можете использовать любые фразы, или воспользоваться нашей подборкой:
@@ -38,9 +38,74 @@
 # экземпляр класса Person должен поочередно взаимодействовать с объектами Car, Boat, Electroscooter
 
 # код должен выполняться не выбрасывая исключений
+from abc import ABC, abstractmethod
 
-# TODO напишите Ваш код здесь
 
+class Transport(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Boat(Transport):
+    def start_engine(self):
+        print(f'Двигатель запущен. Катер затарахтел своим дизельным агрегатом')
+
+    def stop_engine(self):
+        print(f'Двигатель катера чихнул напоследок и заглох')
+
+    def move(self):
+        print(f'Катер быстро набирает скорость')
+
+    def stop(self):
+        print(f'Катер остановился')
+
+
+class Car(Transport):
+    def start_engine(self):
+        print(f'Двигатель запущен. Машина заурчала своим новеньким мотором')
+
+    def stop_engine(self):
+        print(f'Машина стоит с заглушенным двигателем')
+
+    def move(self):
+        print(f'Машина едет к цели назначения')
+
+    def stop(self):
+        print(f'Машина остановилась')
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print(f'Двигатель запущен. Нет топлива, нет звука. Работает бесшумно')
+
+    def stop_engine(self):
+        print(f'Двигатель остановлен. Мигнул светодиодом дважды')
+
+    def move(self):
+        print(f'Прохожие в ужасе разбегаются от очередного камикадзе')
+
+    def stop(self):
+        print(f'Торможение об стену прошло успешно')
+
+class Person:
+    def use_transport(self, transport: Transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
 
 # Отрезок кода для самопроверки.
 # Запустите его, после того как выполните задание
